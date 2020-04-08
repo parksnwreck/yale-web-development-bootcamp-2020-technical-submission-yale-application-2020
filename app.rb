52ba2b56c51a671b587fcbf00ad03102eaa94c58
Jun Park <seongjun.park@yale.edu>
2020-04-08 14:33:11 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,6 +3,12 @@ AVERAGE_HEIGHT_IN_INCHES = 67
 
 # Your Code Here!
 
+name = "Jun Park"
+age = 22
+favorite_food = "enchilada"
+height_in_inches = 67
+height_message = height_in_inches > AVERAGE_HEIGHT_IN_INCHES ? "You are taller than average." : "You are not taller than average."
+
 ## Do not modify below this line!
 
 output(name, age, favorite_food, height_message)
