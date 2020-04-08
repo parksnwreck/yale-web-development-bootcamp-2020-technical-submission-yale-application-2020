ef543cd616ae52576286d08a686715d013c21d2a
Jun Park <seongjun.park@yale.edu>
2020-04-08 14:29:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,6 +3,10 @@ AVERAGE_HEIGHT_IN_INCHES = 67
 
 # Your Code Here!
 
+name = "Jun Park"
+age = 22
+favorite_food = "Enchil"
+
 ## Do not modify below this line!
 
 output(name, age, favorite_food, height_message)
